40147c2931013a93f6c8e055208cf82c35a809de
Veerendras2004 <veerendrasvikky@gmail.com>
2025-05-11 09:21:25 +0530

    feat: update portfolio with latest changes

--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -12,6 +12,8 @@ export const About = () => {
 
   const backendSkills = ["Node.js","MySQL", "MongoDB"];
 
+  const lang =["C","C++","Java","Python",];
+
   return (
     <section
       id="about"
@@ -29,6 +31,22 @@ export const About = () => {
               Passionate developer with expertise in building scalable web
               applications and creating innovative solutions.
             </p>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all items-center">
+                <h3 className="text-xl font-bold mb-4"> Languages</h3>
+                <div className="flex flex-wrap gap-2">
+                  {lang.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
@@ -63,21 +81,7 @@ export const About = () => {
                 </div>
               </div>
 
-                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all items-center">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
+               
 
             </div>
           </div>
