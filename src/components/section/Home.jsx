a095bdc223dcd7c76667efd8f4d9440ad51cf3a4
Veerendras2004 <veerendrasvikky@gmail.com>
2025-05-11 09:24:56 +0530

    languages

--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -31,7 +31,7 @@ export const Home = () => {
               href="https://drive.google.com/file/d/1q0n8VvKvTsP2s4rOQ__OF44tMxjp9qIo/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4) justify-center]"
             >
               Resume
             </a>
