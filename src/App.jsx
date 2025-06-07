643e70f23a8733f042e167999e6ce0f8e469b360
Veerendras2004 <veerendrasvikky@gmail.com>
2025-06-07 08:28:17 +0530

    dotenv

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-require('dotenv').config();
 import './App.css'
 import "./index.css"
 import { MobileMenu } from './components/MobileMenu'
